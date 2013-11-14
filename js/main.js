c4e36df41f99e461e0fca02ceb0992ce58ca5c43
Vova <metal_god@ovi.com>
2013-11-14 16:42:18 +0200

    minor changes

--- js/main.js
+++ js/main.js
@@ -1,54 +1,49 @@
 window.onload = function(){
-	// (function(){
-		window.STAR = {
+	(function(){
+		var STAR = {
 				smallActive : "sprite-star_100_per_cent",
 				bigActive : "sprite-star_big_100_per_cent",
 				smallNon : "sprite-star_0_per_cent",
 				bigNon : "sprite-star_big_0_per_cent",
 				smallStars : document.getElementsByClassName("sprite-star_0_per_cent"),
-				bigStars : document.getElementsByClassName("sprite-star_big_0_per_cent"),
-				dropDowns : document.getElementsByTagName("select"),
+				bigStars : document.getElementsByClassName("sprite-star_big_0_per_cent")
 		}
-		var arr = [];
-		// var lenghDD = STAR.dropDowns.length;
-		for(var i = 0; i < STAR.dropDowns.length; i++){
-			arr[i] = STAR.dropDowns[i].getElementsByTagName("option");
-			console.log("----------------")
-			for(var j = 0; j < arr[i].length; j++){
-				arr[i][j].addEventListener("mouseover", function(){
-					this.style.color = "#fef";
-					this.style.background = "#002134";
-				});
-				arr[i][j].addEventListener("mouseout", function(){
-					this.style.color = "#edc";
-				});
+		function setStarActive(obj){
+			var _class, _object, _newClass;
+			switch(obj.typeStars){
+			case "small" : _class = STAR.smallNon;
+							_object = STAR.smallStars;
+							_newClass = STAR.smallActive;
+			break;
+			case "big" : _class = STAR.bigStars;
+						_object = STAR.bigStars;
+						_newClass = STAR.bigNon;
+			}
+			setStarUnactive();
+			for(var i = 0; i <= obj.counter; i++){
+				var regExp = /\b_newClass\b/;
+				_object[i].className = _object[i].className.replace(_class,_newClass);				
 			}
 		}
-		// function _addIndexes(element, _index){
-		// 	console.log(element + " " + _index);
-		// 	element.className += "ind" + _index;
-		// }
+		function setStarUnactive () {
+			var _classAct = STAR.smallActive;
+			for(var i = 0; i < STAR.smallStars.length; i++){
+				STAR.smallStars[i].className = STAR.smallStars[i]
+													.className.replace(STAR.smallActive,STAR.smallNon);
+			}
+		}
+		for(var i = 0; i < STAR.smallStars.length; i++){
+			STAR.smallStars[i].className += " st" + i;
+		}
+		for(var i = 0; i < STAR.smallStars.length; i++){
+			STAR.smallStars[i].addEventListener("click",function(){
+				var regExp = /\bst[0-9]\b/, 
+					a = (this.className.match(regExp)).toString().substring(2);
+				setStarActive({counter : a, typeStars : "small"});
+			});
+		}
 
-		// function _addActiveState(element, _class, newClass){
-		// 	element.className = element.className.replace(_class, newClass);
-		// }
-
-		// for(var i = 0; i < STAR.bigStars.length; i++){
-		// 	var st = STAR.bigStars[i];
-		// 	_addIndexes(st,i);
-		// 	st.addEventListener("click", function(){
-		// 		// _addActiveState(this,STAR.bigNon, STAR.bigActive);
-		// 		// this = this.previousSibling;
-		// 		// console.log(this.indexOf);
-		// 		// _addActiveState(this,STAR.bigNon, STAR.bigActive);
-		// 	});
-		// }
-		// for(var i = 0; i < STAR.smallStars.length; i++){
-		// 	STAR.smallStars[i].addEventListener("click", function(){
-		// 		_addActiveState(this,STAR.smallNon, STAR.smallActive);
-		// 	});
-		// }
-	// }());
+	}());
 }
 
 	// }
